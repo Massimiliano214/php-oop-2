699b82ed6e283929a374ead1dfc866a1c6ff7be8
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-05 15:40:44 +0200

    created products

--- Models/Cat.php
+++ Models/Cat.php
@@ -1,5 +1,10 @@
 <?php
 
     class Cat extends Product{
+
+        public function __construct($item, $price, $type, $genre)
+        {
+            parent::__construct($item, $price, $type, $genre);
+        }
             
     }
--- index.php
+++ index.php
@@ -4,4 +4,22 @@
     require_once __DIR__ . '/Models/Dog.php';
     require_once __DIR__ . '/Models/Cat.php';
 
-    
+    $food = new Dog('Croccantini', '13,50 $', 'cibo', '300 calorie');
+
+    var_dump($food);
+
+    $catNip = new Cat('Cat Nip', '5,00 $', 'droga', 'intrattenimento');
+
+    var_dump($catNip);
+
+    $game = new Cat('Gioco', '10,00 $', 'giocattolo', 'intrattenimento');
+
+    var_dump($game);
+
+    $leash = new Dog('Guinzaglio', '20,00 $', 'utilità', 'neccessità');
+
+    var_dump($leash);
+
+    $kennels = new Dog('Cuccia', '50,00 $', 'utilità', 'neccessità');
+
+    var_dump($kennels);
